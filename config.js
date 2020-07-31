0d055f46c2f63ae04c7b6cb712d91cc0788052ae
Hiashura <hiashura1@gmail.com>
2020-07-31 03:23:19 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = "45.77.2.72.vultr.com";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "45.77.2.72.vultr.com";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "tbd";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/xmr/block/";
+
+var transactionExplorer = "http://chainradar.com/xmr/transaction/";
